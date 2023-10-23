06f53222fefb3e1d0fcec253943f3633d6e083ba
crysy-rThomas <Ryan.thomas@iut-dijon.u-bourgogne.fr>
2023-10-23 15:03:35 +0200

    change db

--- timemanager/config/dev.exs
+++ timemanager/config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :timemanager, Timemanager.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "db",
+  hostname: "localhost",
   database: "timemanager",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
