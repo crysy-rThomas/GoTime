87dac5bee4722a77279104f9dff85ef2996f0dfa
crysy-rThomas <Ryan.thomas@iut-dijon.u-bourgogne.fr>
2023-10-30 15:16:58 +0100

    token without bd token

--- timemanager/lib/timemanager_web/controllers/login_controller.ex
+++ timemanager/lib/timemanager_web/controllers/login_controller.ex
@@ -4,12 +4,14 @@ defmodule TimemanagerWeb.LoginController do
   alias Timemanager.Users
   alias Timemanager.Tokens
 
-
   def login(conn, %{"email" => email, "password" => password}) do
       case Users.authenticate_user(email, password) do
         {:ok, user} ->
-          token = Phoenix.Token.sign(TimemanagerWeb.Endpoint, "user auth", user.id)
-          Tokens.create_token(%{token: token, user: user.id})
+          claim = %{"user_id" => user.id, "role" => user.role, "email" => user.email, "firstname" => user.firstname, "lastname" => user.lastname, "age" => DateTime.utc_now()}
+          token = Phoenix.Token.sign(TimemanagerWeb.Endpoint, "user auth", claim)
+          # Tokens.create_token(%{token: token, user: user.id})
+
+
 
           conn
           |> put_status(:ok)
